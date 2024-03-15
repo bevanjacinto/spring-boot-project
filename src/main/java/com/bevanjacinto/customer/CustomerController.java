0e2430b93f8a096877d5b018bcc19991690fa1a2
Bevan Jacinto <bevanjacinto.bj@gmail.com>
2024-03-15 03:16:24 -0700

    Setting up database

---
+++ src/main/java/com/bevanjacinto/customer/CustomerController.java
@@ -0,0 +1,30 @@
+package com.bevanjacinto.customer;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class CustomerController {
+
+    private final CustomerService customerService;
+
+
+    public CustomerController(CustomerService customerService) {
+        this.customerService = customerService;
+    }
+
+    @GetMapping(path = "api/v1/customers")
+    public List<Customer> getCustomers() {
+        return customerService.getAllCustomers();
+    }
+
+    @GetMapping(path = "api/v1/customers/{customerId}")
+    public Customer getCustomer(@PathVariable("customerId") Integer customerId) {
+        return customerService.getCustomer(customerId);
+    }
+
+}
